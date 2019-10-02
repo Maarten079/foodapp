553782d60654c60a450866d9a716ab3ecfe3e7c5
maarten079 <m.moerland.92@gmail.com>
2019-10-02 17:37:37 +0200

    added recipe search form

--- index.php
+++ index.php
@@ -3,12 +3,19 @@
 /**
  * @todo implement api key reader
  */
+if(isset($_GET['recipe'])) {
+    $url = 'https://api.spoonacular.com/recipes/search?query='.$_GET['recipe'].'&number=2&apiKey=';
+    $ch = curl_init($url);
 
-$secret = '';
-$url = 'https://api.spoonacular.com/recipes/search?query=cheese&number=2&apiKey='.$secret;
-$ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    echo $result;
+}
 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$result = curl_exec($ch);
-curl_close($ch);
-echo $result;
+?>
+
+<form action="index.php">
+    <input type="text" name="recipe">Recipe
+    <input type="submit" value="submit">
+</form>
